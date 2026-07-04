9e4c4e064f3e2ae11068d51a5f29c4f8b8e95851
RoyTR <roytazarojas@gmail.com>
2026-07-04 09:07:10 -0500

    agregado ejemplo bd

---
+++ json prueba.py
@@ -0,0 +1,15 @@
+#array - arreglo - vector
+data = [
+    {"id": 1, "name": "Johnas"},
+    {"id": 2, "name": "Jane"},
+    {"id": 3, "name": "Bob"}
+]
+
+nombre = input("Ingrese el nuevo nombre: ")
+
+data.append({"id": 4, "name": nombre})
+
+#print(data[0])
+
+print(data[0])
+
